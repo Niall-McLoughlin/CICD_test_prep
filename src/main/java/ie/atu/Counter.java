f2114f232d515b872bc95c61ac4f4ec89b20c9b2
Niall <G00362680@atu.ie>
2022-12-06 17:43:27 +0000

    Added test class

--- src/main/java/ie/atu/Counter.java
+++ src/main/java/ie/atu/Counter.java
@@ -1,11 +1,18 @@
 package ie.atu;
 
-
-
 public class Counter {
+    private int countValue;
 
-
+    public Counter() {
+        countValue = 0;
     }
+
+    public int increment() {
+        countValue++;
+        return countValue;
+    }
+
+}
 
 
 
--- src/test/java/ie/atu/CounterTest.java
+++ src/test/java/ie/atu/CounterTest.java
@@ -2,14 +2,24 @@ package ie.atu;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
+    Counter myCount;
 
     @BeforeEach
     void setUp() {
+        myCount = new Counter();
     }
+
+    @Test
+    void testIncrement()
+    {
+     assertEquals(1, myCount.increment());
+    }
+
 
     @AfterEach
     void tearDown() {
